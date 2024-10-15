13dd0f737449bbaf125c7ad044662edd4a7a6d9c
João Costa <joaomacostadev@gmail.com>
2024-10-15 11:24:50 +0100

    Payment request

--- VerifoneSPRemotePurchaseTerminalIntegration.Lib/Models/Purchase.cs
+++ VerifoneSPRemotePurchaseTerminalIntegration.Lib/Models/Purchase.cs
@@ -4,7 +4,7 @@ namespace VerifoneSPRemotePurchaseTerminalIntegration.Lib.Models
 {
     internal class Purchase
     {
-        private const string _commandPurchase = "C00010#TRANSACTIONID##AMOUNT#0000#PRINTRECEIPTONPOS#000";
+        private const string _commandPurchase = "1D004753303610#TRANSACTIONID#0101#AMOUNT#30303000";
 
         public string TransactionId { get; set; }
         public string Amount { get; set; }
@@ -13,9 +13,9 @@ namespace VerifoneSPRemotePurchaseTerminalIntegration.Lib.Models
         override public string ToString()
         {
             string command = _commandPurchase
-                .Replace("#TRANSACTIONID#", TransactionId.PadLeft(4, '0'))
-                .Replace("#AMOUNT#", Amount.PadLeft(8, '0'))
-                .Replace("#PRINTRECEIPTONPOS#", Convert.ToByte(PrintReceiptOnPOS).ToString());
+                .Replace("#TRANSACTIONID#", Utilities.ConvertToHexString(TransactionId.PadLeft(10, '0')).Replace(" ", string.Empty))
+                .Replace("#AMOUNT#", Utilities.ConvertToHexString(Amount.PadLeft(8, '0')).Replace(" ", string.Empty))
+                .Replace("#PRINTRECEIPTONPOS#", Convert.ToByte(PrintReceiptOnPOS).ToString().PadLeft(2, '0'));
 
             return command;
         }
