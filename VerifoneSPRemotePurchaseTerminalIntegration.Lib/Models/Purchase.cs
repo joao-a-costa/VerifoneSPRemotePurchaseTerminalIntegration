8ec048cebffad76648bbb8c323727392a1d05e93
João Costa <joaomacostadev@gmail.com>
2024-11-11 10:28:24 +0000

    Fix Option to print on terminal not working.

--- VerifoneSPRemotePurchaseTerminalIntegration.Lib/Models/Purchase.cs
+++ VerifoneSPRemotePurchaseTerminalIntegration.Lib/Models/Purchase.cs
@@ -4,7 +4,7 @@ namespace VerifoneSPRemotePurchaseTerminalIntegration.Lib.Models
 {
     internal class Purchase
     {
-        private const string _commandPurchase = "1D004753303610#TRANSACTIONID#0101#AMOUNT#30303000";
+        private const string _commandPurchase = "1D004753303610#TRANSACTIONID##PRINTRECEIPTONPOS#01#AMOUNT#30303000";
 
         public string TransactionId { get; set; }
         public string Amount { get; set; }
@@ -15,7 +15,7 @@ namespace VerifoneSPRemotePurchaseTerminalIntegration.Lib.Models
             string command = _commandPurchase
                 .Replace("#TRANSACTIONID#", Utilities.ConvertToHexString(TransactionId.PadLeft(10, '0')).Replace(" ", string.Empty))
                 .Replace("#AMOUNT#", Utilities.ConvertToHexString(Amount.PadLeft(8, '0')).Replace(" ", string.Empty))
-                .Replace("#PRINTRECEIPTONPOS#", Convert.ToByte(PrintReceiptOnPOS).ToString().PadLeft(2, '0'));
+                .Replace("#PRINTRECEIPTONPOS#", (Convert.ToByte(PrintReceiptOnPOS) + 1).ToString().PadLeft(2, '0').Replace(" ", string.Empty));
 
             return command;
         }
